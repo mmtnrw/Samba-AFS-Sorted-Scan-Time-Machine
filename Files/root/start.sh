f5c26b3075f20ea48b3cc747042f1cf724e1eaaf
mmtnrw <46136708+mmtnrw@users.noreply.github.com>
2021-01-24 17:16:30 +0100

    Create start.sh

---
+++ Files/root/start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo "[info] Setting up Timezone : $TZ"
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+echo $TZ > /etc/timezone
+
+echo "[info] Syncing Time...."
+ntpd -d -q -n -p time.cloudflare.com &> /dev/null
+
+echo "[info] Starting Cronie....."
+/usr/sbin/crond &
+
+echo "[info] Starting Date Sorted Folder Script...."
+source /root/scan.sh
+
+echo "[info] Updating Adblocking...."
+source /root/scan.sh
+
+/usr/sbin/nmbd -D
+/usr/sbin/avahi-daemon -D
+/usr/sbin/smbd -F
+
+
+#if [[ ! -z "$LISTEN" ]]; then
+#echo "[info] Changing Interface to: $LISTEN"
+#sed -i "1,/interface/ s/interface: .*$/interface: $LISTEN/" /etc/unbound/unbound.conf
+#fi
+#echo "[info] Starting Unbound....."
+#chown -R unbound:unbound /etc/unbound
+#/usr/sbin/unbound -d -vvv
+#echo "[info] Starting Bash for Error searching....."
+#/bin/sh
